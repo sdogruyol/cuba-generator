07385d2d06da6fc8700fdee1bdc08a26586ce433
Sdogruyol <dogruyolserdar@gmail.com>
2015-03-11 11:01:01 +0200

    Added type for new app

--- lib/cuba/generator/cli.rb
+++ lib/cuba/generator/cli.rb
@@ -13,7 +13,7 @@ module Cuba
 
       command :new do |c|
         c.syntax = 'cuba new [options]'
-        c.description = 'Creates an app with preferred type'
+        c.description = 'Creates a new Cuba app'
         c.option '--type STRING', String, 'Creates an app with preferred type'
         c.action do |args, options|
           if options.type
